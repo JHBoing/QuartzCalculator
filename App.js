1bef76cd2965875b8c4781306f4e71d894d4b83f
Joao Boing <joao.boing@vectorinf.com.br>
2019-05-24 17:07:48 -0300

    Changed Button to TouchableOpacity
    Changed Button to TouchableOpacity and fixed ambars count.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
 
@@ -31,88 +31,94 @@ export default class App extends React.Component {
             <View style={styles.container}>
                 <Text>Quartz Calculatora</Text>
                 <View style={styles.pedraContainer}>
-                    <Button
+                    <TouchableOpacity
                         title="-"
                         onPress={(quartzo) => (this.state.quartzo > 0) ? this.setState({quartzo: this.state.quartzo - 1}):null}
                     />
                     <Text>Quartzo</Text>
-                    <Text>{this.state.quartzo}</Text>
-                    <Button
+                    <Text> {this.state.quartzo}</Text>
+                    <TouchableOpacity
                         title="+"
                         onPress={(quartzo) => this.setState({quartzo: this.state.quartzo + 1})}
                     />
                 </View>
                 <View style={styles.pedraContainer}>
-                    <Button
+                    <TouchableOpacity
                         title="-"
                         onPress={(quartzoRosa) => (this.state.quartzoRosa > 0) ? this.setState({quartzoRosa: this.state.quartzoRosa - 1}):null}
                     />
                     <Text>Quartzo Rosa</Text>
-                    <Text>{this.state.quartzoRosa}</Text>
-                    <Button
+                    <Text> {this.state.quartzoRosa}</Text>
+                    <TouchableOpacity
                         title="+"
                         onPress={(quartzoRosa) => this.setState({quartzoRosa: this.state.quartzoRosa + 1})}
                     />
                 </View>
                 <View style={styles.pedraContainer}>
-                    <Button
+                    <TouchableOpacity
                         title="-"
                         onPress={(rubelita) => (this.state.rubelita > 0) ? this.setState({rubelita: this.state.rubelita - 1}):null}
                     />
                     <Text>Rubelita</Text>
-                    <Text>{this.state.rubelita}</Text>
-                    <Button
+                    <Text> {this.state.rubelita}</Text>
+                    <TouchableOpacity
                         title="+"
                         onPress={(rubelita) => this.setState({rubelita: this.state.rubelita + 1})}
                     />
                 </View>
                 <View style={styles.pedraContainer}>
-                    <Button
+                    <TouchableOpacity
                         title="-"
                         onPress={(esmeralda) => (this.state.esmeralda > 0) ? this.setState({esmeralda: this.state.esmeralda - 1}):null}
                     />
                     <Text>Esmeralda</Text>
-                    <Text>{this.state.esmeralda}</Text>
-                    <Button
+                    <Text> {this.state.esmeralda}</Text>
+                    <TouchableOpacity
                         title="+"
                         onPress={(esmeralda) => this.setState({esmeralda: this.state.esmeralda + 1})}
                     />
                 </View>
                 <View style={styles.pedraContainer}>
-                    <Button
+                    <TouchableOpacity
                         title="-"
                         onPress={(safira) => (this.state.safira > 0) ? this.setState({safira: this.state.safira - 1}):null}
                     />
                     <Text>Safira</Text>
-                    <Text>{this.state.safira}</Text>
-                    <Button
+                    <Text> {this.state.safira}</Text>
+                    <TouchableOpacity
                         title="+"
                         onPress={(safira) => this.setState({safira: this.state.safira + 1})}
                     />
                 </View>
                 <View style={styles.pedraContainer}>
-                    <Button
+                    <TouchableOpacity
                         title="-"
                         onPress={(rubi) => (this.state.rubi > 0) ? this.setState({rubi: this.state.rubi - 1}):null}
                     />
                     <Text>Rubi</Text>
-                    <Text>{this.state.rubi}</Text>
-                    <Button
+                    <Text> {this.state.rubi}</Text>
+                    <TouchableOpacity
                         title="+"
                         onPress={(rubi) => this.setState({rubi: this.state.rubi + 1})}
                     />
                 </View>
                 <View style={styles.pedraContainer}>
-                    <Button
+                    <TouchableOpacity
                         title="-"
-                        onPress={(rubi) => (this.state.rubi > 0) ? this.setState({rubi: this.state.rubi - 1}):null}
-                    />
+                        onPress={(ambar) => (this.state.ambar > 0) ? this.setState({ambar: this.state.ambar - 1}):null}
+                        style={styles.button}
+                    >
+                        <Text>-</Text>
+                    </TouchableOpacity>
                     <Text>Âmbar</Text>
-                    <Text>{this.state.rubi}</Text>
-                    <Button
+                    <Text> {this.state.ambar}</Text>
+                    <TouchableOpacity
                         title="+"
-                        onPress={(rubi) => this.setState({rubi: this.state.rubi + 1})}
-                    />
+                        onPress={(ambar) => this.setState({ambar: this.state.ambar + 1})}
+                        style={styles.button}
+                    >
+                        <Text>+</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         );
@@ -130,5 +136,11 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        width: 50,
+        height: 50
     }
 });
